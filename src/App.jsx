fc72e2f711194532cfe2c5b629c5df5f44ee1442
ahmetkvs <ahmetpkavas@gmail.com>
2025-04-08 11:57:55 +0300

    feat: Added basic structure for TopBar component (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import { Rocket } from "lucide-react";
+import TopBar from "./components/layout/TopBar";
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-6xl mb-4">Coming Soon!</h1>
-      <p className="text-xl mb-6">
-        We're working hard to bring you something amazing!
-      </p>
-      <Rocket color="black" size={96} />
-    </div>
-  );
+  return <TopBar />;
 }
 
 export default App;
---
+++ src/components/layout/TopBar.jsx
@@ -0,0 +1,43 @@
+import {
+  Phone,
+  Mail,
+  Instagram,
+  CirclePlay,
+  Facebook,
+  Twitter,
+} from "lucide-react";
+
+function TopBar() {
+  return (
+    <aside className="w-full flex justify-between text-sm py-4 px-4 bg-slate-800 text-white">
+      <div className="flex gap-6">
+        <span className="flex gap-1">
+          <Phone fill="currentColor" />
+          <p>(555) 555-5555</p>
+        </span>
+        <span className="flex gap-1">
+          <Mail />
+          <p>ahmetpkavas@gmail.com</p>
+        </span>
+      </div>
+      <p>Follow Us and get a chance to win 80% off</p>
+      <div className="flex gap-2">
+        <p className="mr-2">Follow us:</p>
+        <a href="#">
+          <Instagram />
+        </a>
+        <a href="#">
+          <CirclePlay />
+        </a>
+        <a href="#">
+          <Facebook />
+        </a>
+        <a href="#">
+          <Twitter fill="currentColor" />
+        </a>
+      </div>
+    </aside>
+  );
+}
+
+export default TopBar;
