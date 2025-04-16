965fa581dd42c8646c781f13c00755145d184c46
ahmetkvs <ahmetpkavas@gmail.com>
2025-04-17 01:51:02 +0300

    feat: Created customer support section and finished contact page (statically) (#9)

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,11 @@
 import ContactHeroSection from "../components/contactSections/ContactHeroSection";
+import CustomerSupportSection from "../components/contactSections/CustomerSupportSection";
 
 function ContactPage() {
   return (
     <>
       <ContactHeroSection />
+      <CustomerSupportSection />
     </>
   );
 }
